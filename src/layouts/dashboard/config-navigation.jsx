02c9e861893e221430396ab60d972c24ebf57f9b
xli340 <xli340@aucklanduni.ac.nz>
2024-01-18 11:44:35 +1300

    update for layout

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -1,15 +1,26 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
 const navConfig = [
   {
     title: 'dashboard',
     path: '/',
+    icon: icon('ic_analytics'),
   },
   {
     title: 'login',
     path: '/login',
+    icon: icon('ic_lock'),
   },
   {
     title: 'Not found',
     path: '/404',
+    icon: icon('ic_disabled'),
   },
 ];
 
